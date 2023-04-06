d5cfa8dc1c189fa531867bf8230d1e63727e90e9
Nikita <87970276+Atikin-NT@users.noreply.github.com>
2023-04-06 14:08:09 +0300

    result

--- solution.py
+++ solution.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 708133213 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    alpha = -45
+    r = math.exp(1)
+    a = np.array([])
+    for el in x:
+        a = np.append(a, 1 / (math.sqrt(2*math.pi) * r) * math.exp(-(el - alpha) / 2*r**2))
+    return np.sum(a)/len(a)
